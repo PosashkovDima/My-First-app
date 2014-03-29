e3eec17f356cb29b237fc4b4f28abf439420a59b
Posashkov Dmitry <k0kak0la1488@gmail.com>
2014-03-29 21:33:48 +0400

    Cut a piece of code in onCreate().

--- Source/MyFirst.app/src/com/example/myfirst/app/DisplayMessageActivity.java
+++ Source/MyFirst.app/src/com/example/myfirst/app/DisplayMessageActivity.java
@@ -1,6 +1,5 @@
 package com.example.myfirst.app;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -17,12 +16,9 @@ public class DisplayMessageActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		Intent intent = getIntent();
-		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
 		TextView textView = new TextView(this);
 		textView.setTextSize(40);
-		textView.setText(message);
+		textView.setText(getIntent().getStringExtra(MainActivity.EXTRA_MESSAGE));
 
 		setContentView(textView);
 	}
